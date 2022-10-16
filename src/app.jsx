424b73e1c01143c40934844e8d0677a7e9a7dc0a
vagsy <jueyingvagsy@163.com>
2022-10-16 20:25:03 +0800

    fix: product & order

--- src/app.jsx
+++ src/app.jsx
@@ -5,22 +5,30 @@ import { BrowserRouter as Router, Switch, Redirect, Route, Link } from 'react-ro
 import Layout from 'component/layout/index.jsx'
 // 页面
 import Home from 'page/home/index.jsx'
-import Login            from 'page/login/index.jsx';
-import UserList         from 'page/user/index.jsx';
-import ErrorPage        from 'page/error/index.jsx';
+import ProductRouter from 'page/product/router.jsx'
+import Login from 'page/login/index.jsx'
+import OrderList from 'page/order/index.jsx'
+import OrderDetail from 'page/order/detail.jsx'
+import UserList from 'page/user/index.jsx'
+import ErrorPage from 'page/error/index.jsx'
 
 class App extends Component {
   render() {
     let LayoutRouter = (
       <Layout> 
-          <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route path="/user/index" component={UserList}/>
-              <Redirect exact from="/user" to="/user/index"/>
-              <Route component={ErrorPage}/>
-          </Switch>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/product" component={ProductRouter}/>
+          <Route path="/product-category" component={ProductRouter}/>
+          <Route path="/order/index" component={OrderList}/>
+          <Route path="/order/detail/:orderNumber" component={OrderDetail}/>
+          <Route path="/user/index" component={UserList}/>
+          <Redirect exact from="/order" to="/order/index"/>
+          <Redirect exact from="/user" to="/user/index"/>
+          <Route component={ErrorPage}/>
+        </Switch>
       </Layout>
-    );
+    )
     return (
       <Router>
         <Switch>
@@ -33,6 +41,6 @@ class App extends Component {
 }
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 )
